0ff1f8f29a486917f9c9cc3e335066c7a9853d56
sofiapichel <sofiapichel1@gmail.com>
2022-09-27 17:11:03 -0300

    Firs commit

---
+++ ZaineGrey.Project/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿let elem = document.querySelector("#Work");
+let nav = document.querySelector(".navbar");
+
+
+window.addEventListener("scroll", () => {
+
+    if (elem.getBoundingClientRect().top <= 0) {
+        nav.classList.add("invert");
+    } else {
+        nav.classList.remove("invert");
+    }
+
+})
