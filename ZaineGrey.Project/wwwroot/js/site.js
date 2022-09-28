703dca9efd1755639ba74d17f87f5eacff9b18e9
sofiapichel <sofiapichel1@gmail.com>
2022-09-28 11:41:37 -0300

    Dark MODE y animaciones en la landing page

--- ZaineGrey.Project/wwwroot/js/site.js
+++ ZaineGrey.Project/wwwroot/js/site.js
@@ -1,13 +1,35 @@
 ﻿let elem = document.querySelector("#Work");
 let nav = document.querySelector(".navbar");
 
+function darkMode() {
+    var elem = document.querySelector("#Work");
+}
 
 window.addEventListener("scroll", () => {
 
-    if (elem.getBoundingClientRect().top <= 0) {
-        nav.classList.add("invert");
-    } else {
-        nav.classList.remove("invert");
+    if (!elem.classList.contains("dark-mode")) {
+
+        if (elem.getBoundingClientRect().top <= 0) {
+            nav.classList.add("invert");
+        } else {
+            nav.classList.remove("invert");
+        }
     }
+})
+
+let btnDark = document.querySelector("#btn-dark");
+
+btnDark.addEventListener("click", () => {
+    elem.classList.toggle("dark-mode");
+
+    if (elem.classList.contains("dark-mode")) {
+        btnDark.querySelector("img.darkLightModeimg").setAttribute("src", "../images/home/Sol.svg")
+        btnDark.classList.add("lightMode")
+    } else {
+        btnDark.querySelector("img.darkLightModeimg").setAttribute("src", "../images/home/Luna.svg")
+        btnDark.classList.remove("lightMode")
+
+    }
+
 
 })
